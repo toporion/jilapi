85d2ed393f61fe3c4e9a0afd7e6ab59f6490b9a5
Salman Haider <netoporion@gmail.com>
2026-01-23 00:41:00 +0600

    login issue

--- frontend/src/hooks/UseAxiosPublic.js
+++ frontend/src/hooks/UseAxiosPublic.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 const UseAxiosPublic = () => {
     const axiosPublic = axios.create({
         // baseURL: 'http://localhost:8080/api/',
+        
         baseURL:"https://jilapi-dx6c.vercel.app/api/",
     })
     return axiosPublic;
